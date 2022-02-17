1379581112ea3aa91980aacda621e81b05ac99e6
baquinn4 <bradquinnsix@gmail.com>
2022-02-17 15:30:12 -0600

    Day 1: into to basic data types

--- Feb-17.cpp
+++ Feb-17.cpp
@@ -5,6 +5,17 @@ using namespace std;
 
 int main()
 {
-	cout << "hello" << endl;
+	string name = "Bob";
+	int age = 35;
+	cout << "A guy named " << name << endl;
+	cout << "Was " << age << " years old" << endl;
+
+	char grade = 'A';
+	string phrase = "Taco Tuesday";
+	int age2 = 50;
+	double gpa = 2.5;
+	bool isTall = false;
+
+
 	return 0;
 }

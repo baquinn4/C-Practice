78ba278fdeb573f55e50eda3f8ea6a2bd7a30bc8
baquinn4 <bradquinnsix@gmail.com>
2022-02-18 23:30:03 -0600

    Day 2: basic math functions

---
+++ Feb-18-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	int wnum = 5;
+	double dnum = 5.5;
+
+	wnum++;
+
+	cout << 10 % 3 << "\n";
+	cout << wnum << "\n";
+	cout << pow(2,5) << "\n";
+	cout << sqrt(28);
+}
+
+//working with numbers
+// basic math functions

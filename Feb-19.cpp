fe2ca9c4bc0304ccb6f9b35eabbd557e194422a0
baquinn4 <bradquinnsix@gmail.com>
2022-02-19 13:40:29 -0600

    Day 3: basic arrays

---
+++ Feb-19.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	int nums[] = {4, 8, 15, 16, 19};
+
+	int nums2[20];
+
+	nums2[5] = 100;
+
+	cout << nums[2];
+	cout << nums2[5];
+}
+
+//c++ basic arrays

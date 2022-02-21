82fc1a440a9a8431067b86665d3538211f06fcc6
baquinn4 <bradquinnsix@gmail.com>
2022-02-20 20:50:13 -0600

    Day 4: pointers

---
+++ Feb-20.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// pointers, memory address
+
+int main(){
+
+	int age = 19;
+
+	// variable storing pointer of int age
+	int *pAge = &age;
+
+
+	double gpa = 2.7;
+	string name = "mike";
+
+	cout << &age;
+}
